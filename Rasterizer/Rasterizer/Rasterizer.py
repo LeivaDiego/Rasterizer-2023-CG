d3b87d535535b0cccf5d4020398114ed94dcffb4
LeivaDiego <lei21752@uvg.edu.gt>
2023-07-31 17:31:17 -0600

    v2.0 Lines
    segunda version de la GL, ahora es capaz de dibujar lineas en la image.
    Extra: algunas texturas simples

--- Rasterizer/Rasterizer/Rasterizer.py
+++ Rasterizer/Rasterizer/Rasterizer.py
@@ -1,14 +1,49 @@
-from gl import Renderer, color
+import random
+from gl import Renderer, V2, color
 
 width = 1920
 height = 1080
 
 rend = Renderer(width,height)
 
-rend.glClearColor(0.5, 0.5, 0.5)
-rend.glClear()
+# Line Pattern
+#for x in range(0, width, 25):
+#    rend.glLine(V2(0,0), V2(x,height - 1))
+#for x in range(0, width, 25):
+#    rend.glLine(V2(0, height - 1), V2(x,0))
 
-rend.glColor(1,1,1)
-rend.glPoint(250,250)
+# Static
+#for x in range(width):
+#    for y in range(height):
+#        if random.random() > 0.5:
+#            rend.glPoint(x,y)
+
+# Color Static
+#for x in range(width):
+#    for y in range(height): 
+#        rend.glPoint(x,y,color(random.random(),
+#                               random.random(),
+#                               random.random()))
+
+# Star
+for x in range(width):
+    for y in range(height):
+        if random.random() > 0.995:
+            size = random.randrange(0,3)
+            brigthness = random.random() / 2 + 0.5
+            starColor = color(brigthness, brigthness, brigthness)
+            if size == 0:
+                rend.glPoint(x,y, starColor)
+            elif size == 1:
+                rend.glPoint(x,y, starColor)
+                rend.glPoint(x+1,y, starColor)
+                rend.glPoint(x,y+1, starColor)
+                rend.glPoint(x+1,y+1, starColor)
+            elif size == 2:
+                rend.glPoint(x,y, starColor)
+                rend.glPoint(x,y+1, starColor)
+                rend.glPoint(x+1,y, starColor)
+                rend.glPoint(x,y-1, starColor)
+                rend.glPoint(x-1,y, starColor)
 
 rend.glFinish("output.bmp")
--- Rasterizer/Rasterizer/gl.py
+++ Rasterizer/Rasterizer/gl.py
@@ -1,4 +1,7 @@
 import struct
+from collections import namedtuple
+
+V2 = namedtuple('Point2', ['x','y'])
 
 def char(c):
     # 1 byte
@@ -25,6 +28,8 @@ class Renderer(object):
         self.glClearColor(0,0,0)
         self.glClear()
 
+        self.glColor(1,1,1)
+
     def glClearColor(self, r, g, b):
         self.clearColor = color(r,g,b)
 
@@ -38,6 +43,71 @@ class Renderer(object):
     def glPoint(self, x, y, clr = None):
         if (0 <= x < self.width) and (0 <= y < self.height):
             self.pixels[x][y] = clr or self.currColor
+
+    def glLine(self, v0, v1, clr = None):
+        # Bresenham line algorithm
+        # y = m*x + b
+
+        #m = (v1.y - v0.y) / (v1.x - v0.x)
+        #y = v0.y
+        #for x in range(v0.x, v1.x+1):
+        #    self.glPoint(x, int(y))
+        #    y += m
+
+        x0 = int(v0.x)
+        x1 = int(v1.x)
+        y0 = int(v0.y)
+        y1 = int(v1.y)
+
+        # Si el punto 0 es igual al punto 1, solo dibuja el punto en v0
+        if x0 == x1 and y0 == y1:
+            self.glPoint(x0,y0)
+            return
+
+        dy = abs(y1 - y0)
+        dx = abs(x1 - x0)
+
+        steep = dy > dx
+
+        # Si la linea tiene pendiente mayor a 1 o menor a -1
+        # intercambia las X por las Y, Y se dibuja la linea
+        # de manera vertical en vez de dibujarla horizontal
+        if steep:
+            x0, y0 = y0, x0
+            x1, y1 = y1, x1
+
+        # Si el punto inicial en X es mayor que el punto final en X,
+        # intercambia los puntos para que la linea se dibuje de
+        # izquierda a derecja
+        if x0 > x1:
+            x0, x1 = x1, x0
+            y0, y1 = y1, y0
+
+        dy = abs(y1 - y0)
+        dx = abs(x1 - x0)
+
+        offset = 0
+        limit = 0.5
+        m = dy / dx
+        y = y0
+
+        for x in range(x0, x1 + 1):
+            if steep:
+                # Dibujar de manera vertical
+                self.glPoint(y, x, clr or self.currColor)
+            else:
+                # Sibujar de manera horizontal
+                self.glPoint(x, y, clr or self.currColor)
+
+            offset += m
+
+            if offset >= limit:
+                if y0 < y1:
+                    y += 1
+                else:
+                    y -= 1
+
+                limit += 1
 
     def glFinish(self, filename):
         with open(filename, "wb") as file:
