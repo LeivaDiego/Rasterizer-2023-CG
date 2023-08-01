77129de11c01545fc5befa7d5bff73cd7deff7ea
LeivaDiego <lei21752@uvg.edu.gt>
2023-08-01 17:36:26 -0600

    Indominus Rex Texture Test
    Prueba de rellenado de textura con modelo de Indominus Rex

--- Rasterizer/Rasterizer/Rasterizer.py
+++ Rasterizer/Rasterizer/Rasterizer.py
@@ -3,8 +3,8 @@ from obj import Obj
 from gl import Renderer
 
 # La resolucion de la imagen generada
-width = 1024    # Ancho en pixeles
-height = 1024   # Alto en pixeles
+width = 4000    # Ancho en pixeles
+height = 2160   # Alto en pixeles
 
 # Inicializar el Renderer con la resolucion definida
 rend = Renderer(width, height)
@@ -14,11 +14,11 @@ rend.fragmentShader = shaders.fragmentShader
 
 
 # Cargar el modelo en el renderizador
-rend.glLoadModel(filename = "model.obj",              # Archivo del modelo a cargar
-                 textureName = "model.bmp",           # Archivo de la textura del modelo
-                 translate = (width/2,height/2,0),    # Cambiar los valores de posicion x,y dependiendo de donde se quiera colocar el modelo (z se deja en 0)
+rend.glLoadModel(filename = "Irex.obj",              # Archivo del modelo a cargar
+                 textureName = "Body-TM_albino.bmp",           # Archivo de la textura del modelo
+                 translate = (width/2,height/6,0),    # Cambiar los valores de posicion x,y dependiendo de donde se quiera colocar el modelo (z se deja en 0)
                  rotate = (0,90,0),                   # Cambiar los valores de los �ngulos EN GRADOS para rotar el modelo en los ejes x,y,z respectivamente
-                 scale = (400,400,400))               # Cambiar los valores para agrandar el modelo en los ejes x,y,z, siendo 1,1,1 su tama�o original
+                 scale = (3,3,3))               # Cambiar los valores para agrandar el modelo en los ejes x,y,z, siendo 1,1,1 su tama�o original
 
 
 # Renderizar el modelo en la imagen
