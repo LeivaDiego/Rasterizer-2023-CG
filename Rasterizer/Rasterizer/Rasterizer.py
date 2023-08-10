becab154d1c279f208509feda762b1720d66f39c
LeivaDiego <lei21752@uvg.edu.gt>
2023-08-10 02:41:11 -0600

    v2.1 Pot Low Angle
    Renderizado con toma low angle de modelo 3d

--- Rasterizer/Rasterizer/Rasterizer.py
+++ Rasterizer/Rasterizer/Rasterizer.py
@@ -16,28 +16,28 @@ rend.fragmentShader = shaders.fragmentShader
 
 
 ## Medium Shot
-rend.glLookAt(camPos=(0, 3, 0),
+#rend.glLookAt(camPos=(0, 3, 0),
+#              eyePos=(0, 0, -5))
+
+
+#rend.glLoadModel(filename = "pot.obj",              
+#                 textureName = "pot.bmp",           
+#                 translate = (0,-2,-5),                
+#                 rotate = (0,0,0),                    
+#                 scale = (8,8,8))
+
+
+
+# Low Angle
+rend.glLookAt(camPos=(1, -5, 0),
               eyePos=(0, 0, -5))
 
 
 rend.glLoadModel(filename = "pot.obj",              
                  textureName = "pot.bmp",           
-                 translate = (0,-2,-5),                
+                 translate = (0,-1,-5),                
                  rotate = (0,0,0),                    
-                 scale = (8,8,8))
-
-
-
-# Low Angle
-#rend.glLookAt(camPos=(1, -3, 0),
-#              eyePos=(0, 0, -5))
-
-
-#rend.glLoadModel(filename = "model.obj",              
-#                 textureName = "model.bmp",           
-#                 translate = (0,0,-5),                
-#                 rotate = (0,0,0),                    
-#                 scale = (2,2,2))
+                 scale = (10,10,10))
 
 
 ## High Angle
@@ -67,5 +67,5 @@ rend.glLoadModel(filename = "pot.obj",
 
 rend.glRender()
 
-rend.glFinish("output1.bmp")
+rend.glFinish("output2.bmp")
 
