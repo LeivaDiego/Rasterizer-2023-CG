92e28ce398a68c691c7cfe862b6f3687bd30ffc4
LeivaDiego <lei21752@uvg.edu.gt>
2023-08-10 00:01:27 -0600

    Math library Documentation
    Agregada documentacion a la libreria de matematicas para hacer mas entendible el funcionamiento del codigo

--- Rasterizer/Rasterizer/myNumpy.py
+++ Rasterizer/Rasterizer/myNumpy.py
@@ -38,7 +38,12 @@ def barycentricCoords(A, B, C, P):
 
 
 def matrix_multiplier(matrixA, matrixB):
-    # Filas y columnas de cada matriz
+    # Multiplica dos matrices y devuelve el resultado
+    # matrixA: Primera matriz (fila x columna)
+    # matrixB: Segunda matriz (fila x columna)
+    # Retorna: Resultado de la multiplicación (fila x columna)
+
+    # Obtener el número de filas y columnas de cada matriz
     rowsA = len(matrixA)
     colsA = len(matrixA[0])
     rowsB = len(matrixB)
@@ -50,6 +55,7 @@ def matrix_multiplier(matrixA, matrixB):
 
     # Crear una matriz vacía para almacenar el resultado
     matrixC = [[0 for _ in range(colsB)] for _ in range(rowsA)]
+
     # Realizar la multiplicación de matrices
     for i in range(rowsA):
         for j in range(colsB):
@@ -59,7 +65,12 @@ def matrix_multiplier(matrixA, matrixB):
 
 
 def matrix_vector_multiplier(matrix, vector):
-    # Tamaños de la matriz y el vector
+    # Multiplica una matriz por un vector y devuelve el resultado
+    # matrix: Matriz de multiplicación (fila x columna)
+    # vector: Vector de multiplicación (1 x columna)
+    # Retorna: Resultado de la multiplicación (1 x columna)
+
+    # Obtener el número de filas y columnas de la matriz y el vector
     rows = len(matrix)
     cols = len(matrix[0])
     size = len(vector)
@@ -68,10 +79,10 @@ def matrix_vector_multiplier(matrix, vector):
     if cols != size:
         raise ValueError("Las dimensiones de la matriz y el vector no permiten su multiplicación.")
 
-    # Se crea el vector vacío para almacenar resultados
+    # Crear un vector vacío para almacenar resultados
     newVector = [0 for _ in range(rows)]
 
-    # Realizar la multiplicación
+    # Realizar la multiplicación de matriz por vector
     for i in range(rows):
         for j in range(cols):
             newVector[i] += matrix[i][j] * vector[j]
@@ -79,13 +90,22 @@ def matrix_vector_multiplier(matrix, vector):
 
 
 def identity_matrix(n):
+    # Genera una matriz identidad de tamaño n x n
+    # n: Tamaño de la matriz
+    # Retorna: Matriz identidad n x n
     return [[1 if i == j else 0 for j in range(n)] for i in range(n)]
 
 
 def inverse_matrix(matrix):
+    # Calcula la matriz inversa usando eliminación gaussiana
+    # matrix: Matriz original a invertir (fila x columna)
+    # Retorna: Matriz inversa resultante (fila x columna)
+
     n = len(matrix)
+    # Crear una matriz aumentada con la matriz original y la matriz identidad
     augmented_matrix = [row + identity_matrix(n)[i] for i, row in enumerate(matrix)]
 
+    # Realizar la eliminación gaussiana para calcular la inversa
     for i in range(n):
         pivot = augmented_matrix[i][i]
         for j in range(2 * n):
@@ -97,14 +117,22 @@ def inverse_matrix(matrix):
                 for j in range(2 * n):
                     augmented_matrix[k][j] -= factor * augmented_matrix[i][j]
 
+    # Extraer la parte de la inversa de la matriz aumentada
     inverse = [row[n:] for row in augmented_matrix]
     return inverse
 
 
 def cross_product(vecA, vecB):
+    # Calcula el producto cruz entre dos vectores en 3D
+    # vecA: Primer vector (3 componentes)
+    # vecB: Segundo vector (3 componentes)
+    # Retorna: Vector resultante del producto cruz (3 componentes)
+
+    # Verificar si los vectores son de 3 componentes
     if len(vecA) != 3 or len(vecB) != 3:
         raise ValueError("Los vectores deben ser de 3 componentes.")
     
+    # Calcular el producto cruz entre dos vectores en 3D
     result = [
         vecA[1] * vecB[2] - vecA[2] * vecB[1],
         vecA[2] * vecB[0] - vecA[0] * vecB[2],
@@ -114,12 +142,25 @@ def cross_product(vecA, vecB):
 
 
 def vector_normalize(vector):
+    # Normaliza un vector dividiendo cada componente por su magnitud
+    # vector: Vector a normalizar
+    # Retorna: Vector normalizado
+
+    # Calcular la magnitud del vector
     magnitude = sqrt(sum(component ** 2 for component in vector))
+    # Normalizar el vector dividiendo cada componente por la magnitud
     normalized_vector = [component / magnitude for component in vector]
     return normalized_vector
 
 
 def subtract_vector(vectorA, vectorB):
+    # Resta dos vectores componente a componente
+    # vectorA: Vector minuendo
+    # vectorB: Vector sustraendo
+    # Retorna: Vector resultante de la resta
+
+    # Verificar si los vectores tienen la misma longitud
     if len(vectorA) != len(vectorB):
         raise ValueError("Los vectores deben tener la misma longitud.")
+    # Restar componente a componente los vectores
     return [a - b for a, b in zip(vectorA, vectorB)]
