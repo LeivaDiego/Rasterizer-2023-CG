84037d34686577ac2c0bc06bc09181752da595ef
LeivaDiego <lei21752@uvg.edu.gt>
2023-08-10 02:24:13 -0600

    v1.1 Low Angle

--- Rasterizer/Rasterizer/Rasterizer.py
+++ Rasterizer/Rasterizer/Rasterizer.py
@@ -16,28 +16,28 @@ rend.fragmentShader = shaders.fragmentShader
 
 
 ## Medium Shot
-rend.glLookAt(camPos=(2, 0, 0),
-              eyePos=(0, 0, -5))
+#rend.glLookAt(camPos=(2, 0, 0),
+#              eyePos=(0, 0, -5))
 
 
-rend.glLoadModel(filename = "Model.obj",              
-                 textureName = "Model.bmp",           
-                 translate = (0,0,-5),                
-                 rotate = (0,0,0),                    
-                 scale = (2,2,2))
+#rend.glLoadModel(filename = "Model.obj",              
+#                 textureName = "Model.bmp",           
+#                 translate = (0,0,-5),                
+#                 rotate = (0,0,0),                    
+#                 scale = (2,2,2))
 
 
 
 # Low Angle
-#rend.glLookAt(camPos=(1, -3, 0),
-#              eyePos=(0, 0, -5))
+rend.glLookAt(camPos=(1, -3, 0),
+              eyePos=(0, 0, -5))
 
 
-#rend.glLoadModel(filename = "model.obj",              
-#                 textureName = "model.bmp",           
-#                 translate = (0,0,-5),                
-#                 rotate = (0,0,0),                    
-#                 scale = (2,2,2))
+rend.glLoadModel(filename = "model.obj",              
+                 textureName = "model.bmp",           
+                 translate = (0,0,-5),                
+                 rotate = (0,0,0),                    
+                 scale = (2,2,2))
 
 
 ## High Angle
