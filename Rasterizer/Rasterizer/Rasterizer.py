8dce08ca7ea42e4ac5fa38bcbae8b55d1d0f341b
LeivaDiego <lei21752@uvg.edu.gt>
2023-08-10 02:34:39 -0600

    v2.0 Pot Medium Shot
    Renderizado con toma medium shot de modelo 3d

--- Rasterizer/Rasterizer/Rasterizer.py
+++ Rasterizer/Rasterizer/Rasterizer.py
@@ -16,15 +16,15 @@ rend.fragmentShader = shaders.fragmentShader
 
 
 ## Medium Shot
-#rend.glLookAt(camPos=(2, 0, 0),
-#              eyePos=(0, 0, -5))
+rend.glLookAt(camPos=(0, 3, 0),
+              eyePos=(0, 0, -5))
 
 
-#rend.glLoadModel(filename = "Model.obj",              
-#                 textureName = "Model.bmp",           
-#                 translate = (0,0,-5),                
-#                 rotate = (0,0,0),                    
-#                 scale = (2,2,2))
+rend.glLoadModel(filename = "pot.obj",              
+                 textureName = "pot.bmp",           
+                 translate = (0,-2,-5),                
+                 rotate = (0,0,0),                    
+                 scale = (8,8,8))
 
 
 
@@ -55,17 +55,17 @@ rend.fragmentShader = shaders.fragmentShader
 
 
 ## Dutch Angle
-rend.glLookAt(camPos=(-2, -1, 0),
-              eyePos=(0, 0, -5))
+#rend.glLookAt(camPos=(-2, -1, 0),
+#              eyePos=(0, 0, -5))
 
 
-rend.glLoadModel(filename = "model.obj",              
-                 textureName = "model.bmp",           
-                 translate = (0,0,-5),                
-                 rotate = (0,0,25),                    
-                 scale = (2,2,2))
+#rend.glLoadModel(filename = "model.obj",              
+#                 textureName = "model.bmp",           
+#                 translate = (0,0,-5),                
+#                 rotate = (0,0,25),                    
+#                 scale = (2,2,2))
 
 rend.glRender()
 
-rend.glFinish("output.bmp")
+rend.glFinish("output1.bmp")
 
