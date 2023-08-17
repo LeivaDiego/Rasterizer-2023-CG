3b59b6dff9884f481879b6b2dcde18237083649c
LeivaDiego <lei21752@uvg.edu.gt>
2023-08-16 21:47:36 -0600

    v9.0 Starman Shader
    Shader que renderiza el modelo con un efecto similar al poder de estrella de mario bros

--- Rasterizer/Rasterizer/shaders.py
+++ Rasterizer/Rasterizer/shaders.py
@@ -1,6 +1,6 @@
 
 from myNumpy import matrix_multiplier, matrix_vector_multiplier, dot_product
-
+from math import sin, pi
 
 def vertexShader(vertex, **kwargs):
 
@@ -274,3 +274,86 @@ def yellowGlowShader(**kwargs):
     if r >= 1.0: r = 1.0
 
     return r, g, b
+
+
+def StarmanShader(**kwargs):
+    # Se extraen los argumentos clave del diccionario kwargs
+    texture = kwargs["texture"]
+    tA, tB, tC = kwargs["texCoords"]
+    nA, nB, nC = kwargs["normals"]
+    dLight = kwargs["dLight"]
+    u, v, w = kwargs["bCoords"]
+    camMatrix = kwargs["camMatrix"]
+
+    # Valores iniciales de color
+    b = 1.0
+    g = 1.0
+    r = 1.0
+
+    # Si hay una textura, se mezcla con el color base
+    if texture is not None:
+        tU = u * tA[0] + v * tB[0] + w * tC[0]
+        tV = u * tA[1] + v * tB[1] + w * tC[1]
+
+        # Se obtiene el color de la textura en las coordenadas tU, tV
+        textureColor = texture.getColor(tU, tV)
+
+        # Se modifica el color base con los componentes de la textura
+        b *= textureColor[2]
+        g *= textureColor[1]
+        r *= textureColor[0]
+
+    # Se calcula la normal para el punto
+    normal = [u * nA[0] + v * nB[0] + w * nC[0],
+              u * nA[1] + v * nB[1] + w * nC[1],
+              u * nA[2] + v * nB[2] + w * nC[2]]
+
+    # Invierte la dirección de la luz para el cálculo de la intensidad
+    dLight = list(dLight)
+    dLight = [-x for x in dLight]
+
+    # Calcula la intensidad de la luz en función de la normal y la dirección de la luz
+    intensity = dot_product(normal, dLight)
+
+    # Si la intensidad es negativa, se establece en cero
+    if intensity <= 0:
+        intensity = 0
+
+    # Se obtiene la dirección hacia adelante de la cámara desde la matriz de la cámara
+    camForward = (camMatrix[0][2],
+                  camMatrix[1][2],
+                  camMatrix[2][2])
+
+    # Calcula la cantidad de resplandor en función de la normal y la dirección de la cámara
+    glowAmount = 1 - dot_product(normal, camForward)
+
+    # Si el resplandor es negativo, se establece en cero
+    if glowAmount <= 0:
+        glowAmount = 0
+
+    # Efecto de brillo arcoíris basado en las coordenadas de textura
+    # Dependiendo de las coordenadas de la diagonal se generan los valores para rojo, verde y azul, que varian a partir de 
+    # un valor sinusoidal, esto para generar un cambio sutil en las fases de colores
+    diagonalValue = (tU + tV) * 0.5                     # Calcula el valor diagonal promedio en coordenadas de textura
+    rGlow = abs(sin(diagonalValue * 2 * pi))            # Calcula el componente rojo del brillo arcoíris
+    gGlow = abs(sin((diagonalValue + 1/3.0) * 2 * pi))  # Calcula el componente verde del brillo arcoíris
+    bGlow = abs(sin((diagonalValue + 2/3.0) * 2 * pi))  # Calcula el componente azul del brillo arcoíris
+
+    # Se agrega el brillo arcoíris al color base, ponderado por el resplandor
+    b += glowAmount * bGlow  # Agrega brillo azul arcoíris con resplandor ponderado
+    g += glowAmount * gGlow  # Agrega brillo verde arcoíris con resplandor ponderado
+    r += glowAmount * rGlow  # Agrega brillo rojo arcoíris con resplandor ponderado
+
+
+    # Asegura que los componentes de color no excedan 1.0
+    if b >= 1.0:
+        b = 1.0
+    if g >= 1.0:
+        g = 1.0
+    if r >= 1.0:
+        r = 1.0
+
+    # Retorna los componentes de color modificados
+    return r, g, b
+
+
